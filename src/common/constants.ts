eebc837c063063506c642b051efd84720e9ec01e
Andrea Storci <s.andrea.storci@gmail.com>
2025-11-17 00:30:18 +0100

    Added styles
    Added style for ButtonsCustom, InputCustom, SearchBar

---
+++ src/common/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Colores de las lineas del metro
+ */
+export const lineColors: Record<string, string> = {
+    '1': '#F04E98',
+    '2': '#005EB8',
+    '3': '#AF9800',
+    '4': '#6BBBAE',
+    '5': '#FFD100',
+    '6': '#DA291C',
+    '7': '#E87722',
+    '8': '#009A44',
+    '9': '#512F2E',
+    'A': '#981D97',
+    'B': '#B1B3B3',
+    '12': '#B0A32A',
+};
